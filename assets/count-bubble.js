8a87b45c490933370d0e8e75bba8d2d6834506aa
Luciana Czikk <lucianaczikk@gmail.com>
2023-03-18 15:18:37 +0800

    Remove zepto

--- assets/count-bubble.js
+++
@@ -1,4 +0,0 @@
-setTimeout(() => {
- document.getElementById('cart-count-header').classList.remove('no-visible');
- document.getElementById('cart-count-drawer').classList.remove('no-visible')
-}, 500);
